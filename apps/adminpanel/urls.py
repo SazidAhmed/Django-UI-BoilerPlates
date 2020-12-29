0a889bc4e77bbdc0c3b23eca4ba84e907c7bbdcd
Sazid Ahmed <sazidahmed.official@gmail.com>
2020-12-29 21:12:55 +0600

    settings added

--- apps/adminpanel/urls.py
+++ apps/adminpanel/urls.py
@@ -1,11 +1,15 @@
 
 from django.urls import path
-from .views import dashboard, settings
+from .views import dashboard, settings, plans, upgradeComplete
 
 urlpatterns = [
     # Admin panel page urls
     path('', dashboard, name='dashboard'),
+    
+    # settings urls
     path('settings', settings, name='settings'),
+    path('settings/upgrage-plans/', plans, name='upgrage-plans'),
+    path('settings/upgradeComplete/', upgradeComplete, name='upgradeComplete'),
 
 
 
--- apps/website/urls.py
+++ apps/website/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
 # from . import views
-from .views import about, contact, home
+from .views import about, contact, plan, home
 urlpatterns = [
     # website page urls
     path('', home, name='frontpage'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
+    path('plan/', plan, name='plan'),
 
     # website apis
 
